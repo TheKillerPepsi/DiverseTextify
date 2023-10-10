f44d77d8b28b5a03eb0cb7ff0c99664c63dce2ce
TheKillerPepsi <pia@pepsisblog.de>
2023-10-10 22:00:48 +0200

    The Arrays get compared. But the website only shows an nongenedered word if its the only word in the array ( Even if its the last word). Dunno why atm :(

--- code.js
+++ code.js
@@ -1,7 +1,39 @@
 function GenderIT() {
 
+    const nongendered = ["Gerätewart", "Abteilungsleiter", "Unbekannter"];
+    const gendered = ["Geräteaufsicht", "Abteilungsleitung", "unbekannte Person" ];
+
+
+        //read the input field
     let input = document.getElementById('input_field').value;
-    let input_2 = "Täääst";
-    document.getElementById('try').innerHTML = input;
+    // convert to a string
+    String(input);
+    //convert string into array
+    //es wird bei jedem Leerzeichen gesplittet, die Angabe ist in den Lerzeichen
+    const myArray = input.split(" ");
+    //document.getElementById("try").innerHTML = myArray[0];
+
+    //mit dem Loop gehe ich durch jedes Wort im Array. in der Console würde jedes Wort ausgeführt werden
+    // let i = 0;
+    // while (i < myArray.length )
+    // {
+    //     document.getElementById("try").innerHTML = myArray[i];
+    //     i++;
+    // }
+
+    let x = 0;
+    while (x < myArray.length ) 
+    {
+        if (myArray[x] == nongendered[x]) {
+            document.getElementById("try").innerHTML = "Nongendered Word!" + myArray[x];
+        } else {
+            document.getElementById("try").innerHTML = "Everything is fine!";
+            
+        }
+        x++;
+    }
+
+    
+
 
 }
